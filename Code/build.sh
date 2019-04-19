e30b737197c8f54fd732ba7d9c4df0040665957f
root <root@bi-iteb-srvr05.engr.uconn.edu>
2019-04-19 00:06:42 -0400

    Update python lib.

--- Code/build.sh
+++ Code/build.sh
@@ -2,7 +2,7 @@
 rm _pyrisc.so
 
 swig -python pyrisc.i
-gcc -march=native -fPIC -c *.c -I/usr/include/python3.5m
+gcc -march=native -fPIC -c *.c -I/usr/include/python3.6m
 ld -shared *.o -o _pyrisc.so
 python3 -c "import pyrisc"
 
