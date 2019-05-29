2835840813ae9aa4175422fff947e083ede1c98f
chunjiangzhu <36064071+chunjiangzhu@users.noreply.github.com>
2019-05-29 12:09:43 -0400

    Update python lib.

--- Code/build.sh
+++ Code/build.sh
@@ -5,7 +5,7 @@ swig -python pyrisc.i
 # need to set up the python library containing "Python.h"
 #gcc -march=native -fPIC -c *.c -I/opt/anaconda3/envs/python3/include/python3.5m/
 #gcc -march=native -fPIC -c *.c -I/usr/include/python3.6m
-gcc -march=native -fPIC -c *.c -I/usr/include/python3.5m
+gcc -march=native -fPIC -c *.c -I/usr/include/python3.4m
 ld -shared *.o -o _pyrisc.so
 python3 -c "import pyrisc"
 
